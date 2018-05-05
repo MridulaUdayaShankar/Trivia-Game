cf810db9566a7b7b19fd8e344fe1ef629981d9d1
Mridula Udaya Shankar <mridu.sh92@gmail.com>
2018-05-04 21:48:49 -0700

    Added CSS styling and some changes in html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -56,89 +56,73 @@ $(document).ready(function() {
   var index = 0;
   var questionIndex = 0;
   var maxQuestions = questionsList.length;
-  
 
-  function renderQuestion (index) {
+  var intervalId;
+  var clockRunning = false;
+  var time = 0;
+
+  function reset() {
+    time = 0;
+    $("#display").text("00:00");
+  }
+
+  function start() {
+    
+    if (!clockRunning) {
+      intervalId = setInterval(count(), 10000);
+      clockRunning = true;
+    }
+    index++;
+    renderQuestion(index);
+  }
+
+  function stop() {
+    
+    clearInterval(intervalId);
+    clockRunning = false;
+  }
+  
+  function count() {
+    time++;
+    var converted = timeConverter(time);
+    $("#display").text(converted);
+  }
+
+  function timeConverter(t) {
+    var minutes = Math.floor(t / 60);
+    var seconds = t - minutes * 60;
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+      minutes = "00";
+    } else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+  }
+
+  function renderQuestion(index) {
+    if (index >= maxQuestions) {
+      index = 0;
+    }
     var currentQuestion = questionsList[index];
-    console.log('currentQuestion', currentQuestion);
-    var qContainer = $('#question');
-    qContainer.append($('<p>').html(currentQuestion.q));
+    var qContainer = $("#question");
+    qContainer.empty();
+    qContainer.append($("<p>").html(currentQuestion.q));
 
     currentQuestion.options.forEach(function(o) {
-        $('<span>').html(o).appendTo(qContainer);
+      $("<div>").html(o).appendTo(qContainer);
     });
   }
 
-    $("#start").on("click", function(){
-
-  
-  //  Variable that will hold our setInterval that runs the stopwatch
-  var intervalId;
-  
-  // prevents the clock from being sped up unnecessarily
-  var clockRunning = false;
-  
-  // Our stopwatch object
-  var stopwatch = {
-  
-    time: 0,
-  
-    reset: function() {
-  
-      stopwatch.time = 0;
-  
-      // DONE: Change the "display" div to "00:00."
-      $("#display").text("00:00");
-  
-    },
-    start: function() {
-  
-      // DONE: Use setInterval to start the count here and set the clock to running.
-      if (!clockRunning) {
-        intervalId = setInterval(stopwatch.count, 1000);
-        clockRunning = true;
-      }
-    renderQuestion();
-
-    },
-    stop: function() {
-  
-      // DONE: Use clearInterval to stop the count here and set the clock to not be running.
-      clearInterval(intervalId);
-      clockRunning = false;
-    },
-    count: function() {
-  
-      // DONE: increment time by 1, remember we cant use "this" here.
-      stopwatch.time++;
-  
-      // DONE: Get the current time, pass that into the stopwatch.timeConverter function,
-      //       and save the result in a variable.
-      var converted = stopwatch.timeConverter(stopwatch.time);
-      console.log(converted);
-  
-      // DONE: Use the variable we just created to show the converted time in the "display" div.
-      $("#display").text(converted);
-    },
-    timeConverter: function(t) {
-  
-      var minutes = Math.floor(t / 60);
-      var seconds = t - (minutes * 60);
-  
-      if (seconds < 10) {
-        seconds = "0" + seconds;
-      }
-  
-      if (minutes === 0) {
-        minutes = "00";
-      }
-      else if (minutes < 10) {
-        minutes = "0" + minutes;
-      }
-  
-      return minutes + ":" + seconds;
-    }
-  };
-    });
-
+  $("#start").on("click", function() {
+    renderQuestion(index);
+    // clockRunning = false;
+    // time = 0;
+    start();
+  });
 });
